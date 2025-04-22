8fa144366adff4584d50a0387944e4b8e54e9de5
AnicetJonhia <anicet22.aps2a@gmail.com>
2025-04-22 19:06:06 +0300

    updtate about section

--- src/components/sections/about-section.tsx
+++ src/components/sections/about-section.tsx
@@ -7,13 +7,10 @@ export function AboutSection() {
       <div className="mt-8 bg-white dark:bg-gray-800 rounded-xl shadow-md p-6 md:p-8">
         <p className="text-lg leading-relaxed mb-4">
           My name is Tokiniaina Jean Anicet Jonhia RANDRIANAMBININA. I come from Fianarantsoa, Madagascar.
-        </p>
-        <p className="text-lg leading-relaxed">
-          🤖 I specialize in full-stack development, working with modern technologies such as React, Django, and various
-          cloud services. With extensive experience in building scalable and responsive web applications, I focus on
-          delivering high-quality solutions that meet both client and user needs. I am constantly learning and
-          applying new techniques in software architecture and AI to stay ahead of the curve.
-        </p>
+        </p><p className="text-lg leading-relaxed">
+              🤖 Full-stack developer specializing in React, Django, and cloud services. Experienced in building scalable, responsive web apps, with a focus on high-quality solutions. Constantly learning and applying new techniques in software architecture and AI.
+            </p>
+
       </div>
     </section>
   )
