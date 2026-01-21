3ef27137aebb5eb56175bf660a6e21e4ab825188
AnicetJonhia <anicet22.aps2a@gmail.com>
2026-01-21 23:59:50 +0300

    lib social

---
+++ src/lib/contact.ts
@@ -0,0 +1,7 @@
+
+
+export const SOCIAL_HREFS = {
+  github: process.env.NEXT_PUBLIC_GITHUB_URL || "",
+  linkedin: process.env.NEXT_PUBLIC_LINKEDIN_URL || "",
+  facebook: process.env.NEXT_PUBLIC_FACEBOOK_URL || "",
+};
